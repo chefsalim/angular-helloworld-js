c32542a4e6367c3e5e7aabc2547ad2ea92027974
Salim Alam <salam@chef.io>
2016-09-21 13:17:17 -0700

    Use UpgradeAdapter to bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import angular from 'angular';
 
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import { UpgradeAdapter } from '@angular/upgrade';
+
 import {hello} from './app/hello';
 import 'angular-ui-router';
 import routesConfig from './routes';
@@ -12,3 +16,6 @@ angular
   .module(app, ['ui.router'])
   .config(routesConfig)
   .component('app', hello);
+
+const upgradeAdapter = new UpgradeAdapter();
+upgradeAdapter.bootstrap(document.body, ['app'], {strictDi: false});
